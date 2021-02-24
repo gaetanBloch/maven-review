42010dd97c8173059554fa60612b2b43f7494322
gbloch <gaetan.bloch@gmail.com>
2021-02-24 09:56:22 +0100

    Introduction to Maven

---
+++ src/test/java/com/gbloch/MainTest.java
@@ -0,0 +1,15 @@
+package com.gbloch;
+
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void fakeTest1() {
+        System.out.println("--- TEST 1 ---");
+    }
+
+    @Test
+    public void fakeTest2() {
+        System.out.println("--- TEST 2 ---");
+    }
+}
